eb03baa41112155b48fb4ba5992ecf602086f95b
Sania Alinejad <152985607+saniaalinejad@users.noreply.github.com>
2024-04-19 16:16:20 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+let audioElem = document.querySelector('audio')
+
+let musicsSrc = [
+    'media/html.m4a',
+    'media/kar.m4a',
+    'media/bazar.m4a'
+]
+
+let audioIndex = 0
+
+function previousMusicHandler () {
+    audioIndex--
+
+    if (audioIndex < 0) {
+        audioIndex = 2
+    }
+
+    audioElem.setAttribute('src', musicsSrc[audioIndex])
+    console.log(musicsSrc[audioIndex]);
+    playHandler()
+}
+
+function playHandler () {
+    audioElem.play()
+    console.log('Music Played');
+
+    setInterval(function () {
+        console.log(audioElem.currentTime);
+    }, 1000);
+}
+
+function pauseHandler () {
+    audioElem.pause()
+    console.log('Music Paused');
+}
+
+function nextMusicHandler () {
+    audioIndex++
+
+    if (audioIndex > musicsSrc.length - 1) {
+        audioIndex = 0
+    }
+
+    audioElem.setAttribute('src', musicsSrc[audioIndex])
+    console.log(musicsSrc[audioIndex]);
+    playHandler()
+}
+
+function musicSpeedHandler () {
+    audioElem.playbackRate = 2
+    console.log('Speed 2X');
+}
+
+function timeMinuMusicHandler () {
+    audioElem.currentTime -= 5
+}
+
+function timePlusMusicHandler () {
+    audioElem.currentTime += 5
+}
